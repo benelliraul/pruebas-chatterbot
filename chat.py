d0e18c7b7017b1a16d1fe06d68b3cf8ec7adca25
benelliraul <47056733+benelliraul@users.noreply.github.com>
2019-02-12 12:25:18 -0300

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,22 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import logging 
+from chatterbot.response_selection import get_most_frequent_response
+
+logger = logging.getLogger() 
+logger.setLevel(logging.CRITICAL)
+chatbot = ChatBot("Ejemplo de bot", read_only=True)
+trainer = ChatterBotCorpusTrainer(chatbot)
+#trainer.train(
+    './preguntas_varias.yml'
+
+)
+usuario = " "
+
+while usuario != "exit":
+    usuario = input (">>>")
+
+    if usuario != 'exit':
+        
+        respuesta = chatbot.get_response(usuario)
+        print ("BOT: " +str(respuesta))
